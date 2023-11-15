26b7d1e5ffc115a24cb62cbad5c9e277a838471f
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-11-15 13:57:11 +0530

    Add prod..

--- src/components/AddProduct.js
+++ src/components/AddProduct.js
@@ -1,34 +1,47 @@
-import React, {useState} from 'react'
-import {useNavigate} from "react-router-dom"
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const AddProduct = ({socket}) => {
-  const [name, setName] = useState("")
-  const [price, setPrice] = useState(0)
-  const navigate = useNavigate()
+const AddProduct = ({ socket }) => {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState(0);
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    socket.emit('addProduct', {name, price, owner: localStorage.getItem("userName")});
-    navigate("/products")
-  }
-  
+    e.preventDefault();
+    socket.emit("addProduct", {
+      name,
+      price,
+      owner: localStorage.getItem("userName"),
+    });
+    navigate("/products");
+  };
+
   return (
     <div>
-      <div className='addproduct__container'>
-        <h2>Add a new product</h2>
-        <form className="addProduct__form" onSubmit={handleSubmit}>
-          <label htmlFor='name'>Name of the product</label>
-          <input type="text" name='name' value={name} onChange={e => setName(e.target.value)} required/>
+      <h2>Add a new product</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name of the product</label>
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
 
-          <label htmlFor='price'>Starting price</label>
-          <input type="number" name='price' value={price} onChange={e => setPrice(e.target.value)} required/>
+        <label htmlFor="price">Starting price</label>
+        <input
+          type="number"
+          name="price"
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+          required
+        />
 
-          <button className='addProduct__cta'>SEND</button>
-        </form>
-      </div>
-      
+        <button>SEND</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default AddProduct
+export default AddProduct;
