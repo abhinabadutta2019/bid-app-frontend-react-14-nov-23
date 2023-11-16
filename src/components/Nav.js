11ecc1878852236a8d00223cf9fbeb78c74f0f87
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-11-16 17:49:36 +0530

    made change

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,31 +1,43 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 
-const Nav = ({header, socket}) => {
-  const [notification, setNotification] = useState("")
+const Nav = ({ header, socket }) => {
+  const [notification, setNotification] = useState("");
 
-  useEffect(()=> {
-    socket.on("addProductResponse", data => {
-      setNotification(`@${data.owner} just added ${data.name} worth $${Number(data.price).toLocaleString()}`)
-    })
-  }, [socket])
-  
-  useEffect(()=> {
-    socket.on("bidProductResponse", data => {
-      setNotification(`@${data.last_bidder} just bid ${data.name} for $${Number(data.amount).toLocaleString()}`)
-    })
-  }, [socket])
+  // console.log(socket, "socket");
+
+  useEffect(() => {
+    socket.on("addProductResponse", (data) => {
+      // console.log("addProductResponse data:", data); // Check if data is received
+      setNotification(
+        `@${data.owner} just added ${data.name} worth $${Number(
+          data.price
+        ).toLocaleString()}`
+      );
+    });
+  }, [socket]);
+
+  useEffect(() => {
+    socket.on("bidProductResponse", (data) => {
+      //   console.log("bidProductResponse data:", data); // Check if data is received
+      setNotification(
+        `@${data.last_bidder} just bid ${data.name} for $${Number(
+          data.amount
+        ).toLocaleString()}`
+      );
+    });
+  }, [socket]);
 
   return (
-    <nav className='navbar'>
-      <div className='header'>
+    <nav className="navbar">
+      <div className="header">
         <h2>{header}</h2>
       </div>
-            
-      <div>
-        <p style={{color: "red"}}>{notification}</p>
-      </div>       
-    </nav>
-  )
-}
 
-export default Nav
+      <div>
+        <p style={{ color: "red" }}>{notification}</p>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
