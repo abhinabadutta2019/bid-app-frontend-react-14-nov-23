74f16a7d69ef64d6bfa6695727304591b675a4f3
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-11-18 21:59:44 +0530

    dd

--- src/App.js
+++ src/App.js
@@ -1,5 +1,50 @@
+//
+import React, { useEffect, useState } from "react";
+//
+
 function App() {
-  return <>Hi1</>;
+  //
+  const [data, setData] = useState([]);
+  //
+  useEffect(() => {
+    // Fetch data from the backend when the component mounts
+    fetchData();
+  }, []);
+
+  //
+  const fetchData = async () => {
+    try {
+      // Make a GET request to your backend API
+      const response = await fetch("http://localhost:4000/api");
+
+      // Parse the response as JSON
+      const jsonData = await response.json();
+
+      setData(jsonData.products);
+
+      //
+      // Log the updated state in a useEffect hook
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+  //
+  //
+  return (
+    <div>
+      <h2>All products</h2>
+      <ul>
+        {data.map((item, index) => (
+          <li key={index}>
+            <h4>{item.name}</h4>
+            <p>{item.price}</p>
+            <p>{item.owner}</p>
+            <p>{item.last_bidder}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
